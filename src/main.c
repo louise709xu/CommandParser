18888ac99f3e28ed1efdecd01a8ac054e90c0731
unknown <louise709xu@yahoo.com>
2018-08-08 13:58:06 -0700

    changed blocking receive to non-blocking receive

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 // Define receive/transmit operation queues
 DEFINE_BUF_QUEUE(EMDRV_UARTDRV_MAX_CONCURRENT_RX_BUFS, rxBufferQueue);
 DEFINE_BUF_QUEUE(EMDRV_UARTDRV_MAX_CONCURRENT_TX_BUFS, txBufferQueue);
+
 // Configuration for USART0, location 0
 #define MY_UART                                     \
 {                                                   \
@@ -22,11 +23,16 @@ DEFINE_BUF_QUEUE(EMDRV_UARTDRV_MAX_CONCURRENT_TX_BUFS, txBufferQueue);
   (UARTDRV_Buffer_FifoQueue_t *)&rxBufferQueue,     \
   (UARTDRV_Buffer_FifoQueue_t *)&txBufferQueue      \
 }
+
 UARTDRV_HandleData_t handleData;
 UARTDRV_Handle_t handle = &handleData;
 
 uint8_t txBuffer[64];
 uint8_t rxBuffer[64];
+
+uint32_t cmmd_code = 'tiuq';
+char* commandBuffer_ptr = (char*)&cmmd_code;
+int i = 0;
 
 void callback_TX(UARTDRV_Handle_t handle,
               Ecode_t transferStatus,
@@ -49,45 +55,48 @@ void callback_RX(UARTDRV_Handle_t handle,
   (void)data;
   (void)transferCount;
 
-  //UARTDRV_Transmit(handle, data, 1, callback);
+  UARTDRV_Transmit(handle, data, 1, callback_TX);
+
+  if(rxBuffer[0] != '\n' && rxBuffer[0] != '\r'){
+    commandBuffer_ptr[i%4] = rxBuffer[0];
+    i++;
+  }
+  else {
+	i = 0;
+
+	switch(cmmd_code) {
+	  case 'tiuq':
+	    UARTDRV_Transmit(handle, txBuffer, strlen(txBuffer), callback_TX);
+	    break;
+
+	  default:
+	    break;
+    }
+  }
+
+  UARTDRV_Receive(handle, rxBuffer, 1, callback_RX);
 }
 
 int main(void) {
-  uint32_t cmmd_code = 'tiuq';
-  char* commandBuffer_ptr;
-  commandBuffer_ptr = (char*)&cmmd_code;
   UARTDRV_InitUart_t initData = MY_UART;
   UARTDRV_InitUart(handle, &initData);
 
-  sprintf(&txBuffer[0], "Enter command: \n\r");
+  sprintf(&txBuffer[0], "Start program? (y/n): ");
   UARTDRV_Transmit(handle, txBuffer, strlen(txBuffer), callback_TX);
 
-  int i = 0;
+  UARTDRV_ReceiveB(handle, rxBuffer, 1);
+  UARTDRV_Transmit(handle, rxBuffer, 1, callback_TX);
+
+
+  if(rxBuffer[0] == 'y'){
+    sprintf(&txBuffer[0], "Enter Command: ");
+    UARTDRV_Transmit(handle, txBuffer, strlen(txBuffer), callback_TX);
+	UARTDRV_Receive(handle, rxBuffer, 1, callback_RX);
+  }
 
   while(1){
-    UARTDRV_ReceiveB(handle, rxBuffer, 1);
-	UARTDRV_Transmit(handle, rxBuffer, 1, callback_TX);
-
-	if(rxBuffer[0] != '\n' && rxBuffer[0] != '\r'){
-      commandBuffer_ptr[i%4] = rxBuffer[0];
-      i++;
-	}
-	else {
-	  i = 0;
-	  /*
-	  if(strcmp(commandBuffer, "quit") == 0){
-	    UARTDRV_Transmit(handle, txBuffer, strlen(txBuffer), callback_TX);
-	  }
-	  */
-	  switch(cmmd_code) {
-	    case 'tiuq':
-	      UARTDRV_Transmit(handle, txBuffer, strlen(txBuffer), callback_TX);
-		  break;
-
-	    default:
-	      break;
-      }
-	}
+    //UARTDRV_ReceiveB(handle, rxBuffer, 1);
+	//UARTDRV_Transmit(handle, rxBuffer, 1, callback_TX);
   }
 
 }
